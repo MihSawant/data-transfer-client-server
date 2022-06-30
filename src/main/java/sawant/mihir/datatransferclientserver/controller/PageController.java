8e0f0836fdde4f5ac768acc05b9b66a4acb0ecc5
Mihir Sawant <mih3190Sawant@outlook.com>
2022-06-30 09:09:21 +0530

    Transferring data from client to server using query-params

--- src/main/java/sawant/mihir/datatransferclientserver/controller/PageController.java
+++ src/main/java/sawant/mihir/datatransferclientserver/controller/PageController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public record PageController() {
@@ -15,5 +16,16 @@ public record PageController() {
         page.addAttribute("bookId", id);
         page.addAttribute("bookName", name);
         return "books";
+    }
+
+    @RequestMapping("/mobiles/price")
+    public String getMobile(@RequestParam(required = false) Integer min,
+                            @RequestParam(required = false) Integer max,
+                            @RequestParam(required = false) Boolean isAvailable, Model model){
+        model.addAttribute("minPrice", min);
+        model.addAttribute("maxPrice", max);
+        model.addAttribute("isAvailable", isAvailable);
+
+        return "mobiles";
     }
 }
